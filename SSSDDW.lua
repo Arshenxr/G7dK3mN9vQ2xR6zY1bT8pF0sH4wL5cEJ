6c433de0355e9829e2533894769c7f6107e4b7ee
Arshenxr <fyhn.6bi@gmail.com>
2025-10-27 00:24:51 +0700

    Update SSSDDW.lua

--- SSSDDW.lua
+++ SSSDDW.lua
@@ -1,1 +1,1 @@
-local Westbound = loadstring(game:HttpGet(https://raw.githubusercontent.com/Arshenxr/G7dK3mN9vQ2xR6zY1bT8pF0sH4wL5cEJ/refs/heads/main/script.lua))()
+local Westbound = loadstring(game:HttpGet("https://raw.githubusercontent.com/Arshenxr/G7dK3mN9vQ2xR6zY1bT8pF0sH4wL5cEJ/refs/heads/main/script.lua"))()
